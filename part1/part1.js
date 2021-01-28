5a0be01056ab425222968c723282ddd8a1f3b55d
ethanh6 <38364796+ethanh6@users.noreply.github.com>
2021-01-28 21:03:22 +0800

    update

--- part1/part1.js
+++ part1/part1.js
@@ -29,8 +29,45 @@ let student = {
     courseLoad: ['CSE 110', 'CSE 134', 'VIS 41']
 };
 
-console.log(student["name"]);
-console.log(student["Grad Year"]);
-student.greeting()
-console.log(student["Favorite Teacher"]['name']);
-console.log(student.courseLoad[0]);
+// console.log(student["name"]);
+// console.log(student["Grad Year"]);
+// student.greeting()
+// console.log(student["Favorite Teacher"]['name']);
+// console.log(student.courseLoad[0]);
+
+// console.log(1 == true);
+// console.log(typeof (1 == true));
+
+// if (2 == true) {
+//     console.log("Hello!");
+// } else if (2) {
+//     console.log("How are you?");
+// } else {
+//     console.log("Goodbye");
+// }
+
+function modifyArray(array, callback) {
+    const newArr = [];
+    for (let i=0; i<array.length; i++) {
+        newArr.push(callback(array[i], function (x) {
+            return x * 2;
+        }));
+    }
+    return newArr;
+}
+
+function doSomething (num, callback) {
+    return callback(num + 1);
+}
+
+let abc = modifyArray([1,2,3], doSomething);
+// console.log(abc);
+
+function printNums() {
+    console.log(1);
+    setTimeout(function () {console.log(2);}, 1000);
+    setTimeout(function () {console.log(3);}, 0);
+    console.log(4);
+}
+
+printNums();
--- part1/part1.md
+++ part1/part1.md
@@ -45,4 +45,16 @@
     7.  F. 0
     8.  G. true
 
-16. `==` will try to convert two operands to the same type, then compare the value; `===` will check the type and value of the two operands.
+16. `==` will convert two operands to have the same type before comparison; `===` is a more strict comparison operator where both value and type would be compared.
+
+17. The code would print "How are you?". For the first condition, `2 == true`, it converts `true` to `1`, thus the comparison is `false.
+
+18. ...
+
+19. In this case, `doSomething` is passed into `modifyArray` as a argument. 
+
+- When execution, the for loop would be `newArr.push(doSomething(array[i], function (x) {return x*2}));`. `doSomething` would be called with first argument `array[i]` and second argument`function (x) {return x * 2}` (an anonymous function). 
+
+- Inside `doSomething`, the anonymous function receives parameters `num + 1` and returns `(num + 1) * 2`.
+ 
+-  Back to for loop, the program puts `newArr` the return value, `(num + 1) * 2`. So, with input `[1, 2, 3]`, the program outputs `[4, 6, 8]`.
